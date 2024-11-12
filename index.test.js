7facca2471b250d26f507ab538b40074b55b5514
Andrea M <andrea.ramirez@andrea.ramirez-MacBook-Pro>
2024-11-12 13:37:58 -0800

    added post method

--- index.test.js
+++ index.test.js
@@ -1,43 +1,53 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  test("GET request gets all musicians", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    expect(responseData).toHaveLength(seedMusician.length);
+    expect(response.statusCode).toBe(200);
+  });
+  test("Get a single musician by id", async () => {
+    const response = await request(app).get("/musicians/1");
+    const responseData = JSON.parse(response.text);
+    expect(responseData.name).toBe("Mick Jagger");
+    expect(response.statusCode).toBe(200);
+  });
 
-describe('./musicians endpoint', () => {
-    test("GET request gets all musicians", async () => {
-        const response = await request(app).get("/musicians");
-        const responseData = JSON.parse(response.text);
-        expect(responseData).toHaveLength(seedMusician.length);
-        expect(response.statusCode).toBe(200);
+  test("POST request creates a new musician", async () => {
+    const newMusician = {
+      name: "Freddie Mercury",
+      instrument: "Vocals",
+    };
+    const response = await request(app).post("/musicians").send(newMusician);
+    const responseData = JSON.parse(response.text);
+    expect(responseData.name).toBe("Freddie Mercury");
+    expect(response.statusCode).toBe(201);
+  });
+  
+  test("PUT request updates a musician", async () => {
+    const newMusician = {
+      name: "Freddie Mercury",
+      instrument: "Vocals",
+    };
+    const response = await request(app).put("/musicians/1").send(newMusician);
+    const responseData = JSON.parse(response.text);
+    expect(responseData.name).toBe("Freddie Mercury");
+  });
 
-    })  
-    test("Get a single musician by id", async () => {
-        const response = await request(app).get("/musicians/1");
-        const responseData = JSON.parse(response.text);
-        expect(responseData.name).toBe("Mick Jagger");
-        expect(response.statusCode).toBe(200);
-    })
-    test("PUT request updates a musician", async () => {
-        const newMusician = {
-            name: "Freddie Mercury",
-            instrument: "Vocals"
-        }
-        const response = await request(app).put("/musicians/1").send(newMusician);
-        const responseData = JSON.parse(response.text);
-        expect(responseData.name).toBe("Freddie Mercury");
-    })
-
-    test("DELETE musician by id", async () => {
-        const response = await request(app).delete("/musicians/1");
-        const responseData = JSON.parse(response.text);
-        expect(responseData.message).toBe("Musician with id 1 deleted");
-        expect(response.statusCode).toBe(200);
-    })
-})
+  test("DELETE musician by id", async () => {
+    const response = await request(app).delete("/musicians/1");
+    const responseData = JSON.parse(response.text);
+    expect(responseData.message).toBe("Musician with id 1 deleted");
+    expect(response.statusCode).toBe(200);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,43 +9,46 @@ app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
 app.get("/musicians", async (req, res) => {
-    const musicians = await Musician.findAll();
-    res.json(musicians);
-})
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
 
 app.get("/musicians/:id", async (req, res) => {
-    const id = req.params.id;
-    const musician = await Musician.findByPk(id);
-    res.json(musician);
-})
+  const id = req.params.id;
+  const musician = await Musician.findByPk(id);
+  res.json(musician);
+});
 
 app.put("/musicians/:id", async (req, res) => {
-    const newMusician = req.body;
-    await Musician.update(newMusician, {
-        where: {
-            id: req.params.id
-        }
-    });
-    res.json(newMusician);
-})
+  const newMusician = req.body;
+  await Musician.update(newMusician, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(newMusician);
+});
+
+app.post("/musicians", async (req, res) => {
+  const newMusician = req.body;
+  const musician = await Musician.create(newMusician);
+
+  res.status(201).json(musician);
+});
 
 app.delete("/musicians/:id", async (req, res) => {
-    try {
-        await Musician.destroy({
-            where: {
-                id: req.params.id
-            }
-        });
-        res.json({
-            message: `Musician with id ${req.params.id} deleted`
-        });
-        
-    } catch (error) {
-        console.log(error);
-    }
-    
-})
-
-
+  try {
+    await Musician.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({
+      message: `Musician with id ${req.params.id} deleted`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = app;
